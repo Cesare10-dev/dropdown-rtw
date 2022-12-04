f7d51d732d9f5bf7785aa2e8bb78aa7e60a70ba9
Cesare10-dev <cesare1087@gmail.com>
2022-12-04 18:44:00 +0100

    bg-gradient

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Dropdown from "./Dropdown";
 
 function App() {
   return (
-    <div className="h-screen grid place-items-center">
+    <div className="h-screen grid place-items-center bg-gradient-to-r from-sky-600 to-indigo-600">
       <Dropdown />
     </div>
   );
--- src/Dropdown.jsx
+++ src/Dropdown.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
 import { AiOutlineCaretUp, AiOutlineCaretDown } from "react-icons/ai";
+import list from "./list.json";
 
 function Dropdown() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="relative flex flex-col items-center  w-[300px] h-[300px] border border-red-700">
+    <div className="relative flex flex-col items-center w-[340px] h-[340px] rounded-lg">
       <button
         onClick={() => setIsOpen((prev) => !prev)}
-        className="bg-blue-300 p-4 w-36 flex items-center justify-between"
+        className="bg-blue-400 p-4 w-full flex items-center justify-between font-bold text-lg rounded-lg tracking-wider border-4 border-transparent active:border-white duration-300 active:text-white"
       >
         Dropdown
         {!isOpen ? (
@@ -18,10 +19,19 @@ function Dropdown() {
         )}
       </button>
       {isOpen && (
-        <div className="absolute top-16" value={isOpen}>
-          <h3>Hello 👍🏻</h3>
-          <h3>Hello</h3>
-          <h3>Hello</h3>
+        <div
+          className="bg-blue-400 absolute top-20 flex flex-col items-start rounded-lg p-2 w-full"
+          value={isOpen}
+        >
+          {list.map((item, i) => (
+            <div
+              key={i}
+              className="flex w-full justify-between p-4 hover:bg-blue-300 cursor-pointer rounded-r-lg border-l-transparent hover:border-l-white border-l-4"
+            >
+              <h3 className="font-bold">{item.city}</h3>
+              <h3>{item.emoticon}</h3>
+            </div>
+          ))}
         </div>
       )}
     </div>
